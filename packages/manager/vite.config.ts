8d1d45a43a74d01af327dc9af6aae6b8084a0b2a
bosh-code <ryan@bosher.co.nz>
2025-05-12 09:07:56 +1200

    revert vite changes

--- packages/manager/vite.config.ts
+++ packages/manager/vite.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable node/no-unpublished-import */
-import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
 // Import @philter/common using a relative path. This is a hack, btw.
 // File extension is required to make this work in Node.js v12 AND v14.
@@ -17,14 +17,30 @@ export default defineConfig({
     rollupOptions: {
       input: `./${RELAY_HTML_FILE}`,
       output: {
+        // Do not include chunk hash in bundle file names, as it introduces a
+        // lot of churn whenever the file changes.
         assetFileNames: 'assets/[name][extname]',
+        // The 'philter-manager' prefix is intented to avoid file name conflicts
+        // with other KoLmafia projects. Although this may be no longer
+        // necessary since r20743, we should keep it around for people using old
+        // versions of KoLmafia.
+        // For more info, see:
+        // - https://kolmafia.us/threads/svn-update-does-not-move-files-in-local-copy.26043/post-162344
+        // - https://kolmafia.us/threads/cargo-shorts-gui.25416/post-162271
         chunkFileNames: 'assets/philter-manager.[name].js',
         entryFileNames: 'assets/[name].js'
       }
     },
     sourcemap: true
   },
-  plugins: [react()],
+  define: Object.fromEntries(
+    Object.entries(process.env)
+      // Exclude environment variables that are not valid JavaScript identifiers
+      // (we ignore names with non-ASCII characters for practical purposes)
+      .filter(([name]) => /[_$a-zA-Z][_$\w]*$/.test(name))
+      .map(([name, value]) => [`process.env.${name}`, JSON.stringify(value)])
+  ),
+  plugins: [reactRefresh()],
   server: {
     open: '/philter-manager.index.html',
     proxy: {
